c5a93f955ac6e90f8f6edee1eeb795db12124734
Duygu <43537425+duygubayram@users.noreply.github.com>
2024-12-31 18:05:44 +0100

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,14 @@
+'''
+definitions of important setting variables (resources etc.)
+create a local settings.py (git ignored) with these variables and their appropriate values
+when introducing new variables, add them to this file with empty or default values and commit
+'''
+
+PATH = "/home1/s5416752/pan_organized/dataset/comp/"
+
+# DATASETS
+TRAIN_DATASET_EN = PATH+'dataset_en_train.json'
+TRAIN_DATASET_ES = 'pan_organized/dataset/original/dataset_es_train.json'
+
+TEST_DATASET_EN = PATH+'dataset_en_test.json'
+TEST_DATASET_ES = '/home1/s5416752/pan_organized/dataset/original/dataset_es_train.json'
