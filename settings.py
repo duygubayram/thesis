98425a08e3025184686acf60135a04cadbb38178
Duygu <43537425+duygubayram@users.noreply.github.com>
2024-12-31 18:09:56 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,11 +4,9 @@ create a local settings.py (git ignored) with these variables and their appropri
 when introducing new variables, add them to this file with empty or default values and commit
 '''
 
-PATH = "/home1/s5416752/pan_organized/dataset/comp/"
-
 # DATASETS
-TRAIN_DATASET_EN = PATH+'dataset_en_train.json'
-TRAIN_DATASET_ES = 'pan_organized/dataset/original/dataset_es_train.json'
+TRAIN_DATASET_EN = ''
+TRAIN_DATASET_ES = ''
 
-TEST_DATASET_EN = PATH+'dataset_en_test.json'
-TEST_DATASET_ES = '/home1/s5416752/pan_organized/dataset/original/dataset_es_train.json'
+TEST_DATASET_EN = ''
+TEST_DATASET_ES = ''
